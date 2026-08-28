fde4efb5b3c7acfb2466b46497034542615f1b19
BIODUN MICHAEL <beordunmichael@gmail.com>
2026-08-28 18:57:37 +0100

    feat: implement hire page, add JobCard component, and configure scraper workflow pipeline

--- apps/web/.next/dev/static/chunks/apps_web_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/apps_web_src_app_globals_1wvyn-h.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/apps_web_src_0xi53or._.js","static/chunks/node_modules_12mmbcn._.js","static/chunks/apps_web_src_0pe8oy0._.css"],
-    source: "entry"
-});
